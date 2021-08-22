f33302cd9dd3850fd74e1979e11262bfb28c8a1d
quantran <quantranlinh.dev@gmail.com>
2021-08-22 16:07:09 +0700

    Add folder structure.

---
+++ local_config.js
@@ -0,0 +1,12 @@
+export const locales = [
+  {
+    locale: "en",
+    img: require("/assets/en.svg")
+  },
+  {
+    locale: "vi",
+    img: require("/assets/vi.svg")
+  },
+];
+
+export const LOCALE_KEY = "locale";
